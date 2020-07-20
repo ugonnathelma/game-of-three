1d58db6949f980ad7f7328a2f2613441199f691c
Ugonna Ofoegbu <ugonnaofoegbu@MacBook-Pro-2.fritz.box>
2020-07-21 01:37:33 +0200

    add theme

---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    primary: "#f0932b",
+    secondary: "#0c2461",
+    secondary1: "#0e2a73",
+    grey: "#a5b1c2",
+    grey1: "#edeff2",
+    green: "#1dd1a1",
+    cyan: "#0abde3",
+  },
+};
