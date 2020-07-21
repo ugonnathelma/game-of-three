3daa7939ed6b63339ac6b7e0f3c376e50e3d4bb5
Ugonna Ofoegbu <ugonnaofoegbu@MacBook-Pro-2.fritz.box>
2020-07-21 03:11:36 +0200

    modify css

---
+++ src/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+  padding: 2em;
+  height: 100%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: calc(100% - 64px);
+  align-items: center;
+  justify-content: center;
+`;
