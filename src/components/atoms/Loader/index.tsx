4ecf417709228d6aba100b565bbd145ce68612e4
Ugonna Ofoegbu <ugonnaofoegbu@MacBook-Pro-2.fritz.box>
2020-07-21 03:11:53 +0200

    modify loader

--- src/components/atoms/Loader/index.tsx
+++ src/components/atoms/Loader/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
+import loader from "../../../assets/loader.gif";
 
-const Loader = () => (
-  <img src={"../../../assets/loader.gif"} alt="Loading..." />
-);
+const Loader = () => <img src={loader} alt="Loading..." />;
 
 export default Loader;
